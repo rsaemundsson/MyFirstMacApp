8e3f0743e91dd8fb9498bb052c9ce7e8f2a8e99f
Rögnvaldur Jóhann Sæmundsson <rjs@rognvaldurs-mbp.lan>
2016-02-06 19:20:25 +0100

    Initial Commit

---
+++ MyFirstMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyFirstMacApp
+//
+//  Created by Rögnvaldur Sæmundsson on 6.2.2016.
+//  Copyright © 2016 Rögnvaldur Sæmundsson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyFirstMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyFirstMacApp
+//
+//  Created by Rögnvaldur Sæmundsson on 6.2.2016.
+//  Copyright © 2016 Rögnvaldur Sæmundsson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
